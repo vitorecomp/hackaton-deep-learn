dbb162eca71d5654b337ba6233e9457fcd86310c
Leonardo Oliveira <adsleonardo.o@gmail.com>
2018-09-23 14:04:31 -0300

    Logo added index page

--- services/application/public/img/Visual Studio Code.app/Contents/Resources/app/out/vs/workbench/services/files/node/watcher/nsfw/watcherApp.nls.js
+++
@@ -1,5 +0,0 @@
-/*!--------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-define("vs/workbench/services/files/node/watcher/nsfw/watcherApp.nls",{"vs/base/node/processes":["Can't execute a shell command on a UNC drive."]});
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/f46c4c469d6e6d8c46f268d1553c5dc4b475840f/core/vs/workbench/services/files/node/watcher/nsfw/watcherApp.nls.js.map
--- services/application/public/img/Visual Studio Code.app/Contents/Resources/app/out/vs/workbench/services/search/node/searchApp.nls.js
+++
@@ -1,5 +0,0 @@
-/*!--------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-define("vs/workbench/services/search/node/searchApp.nls",{"vs/base/common/errorMessage":["{0}: {1}","An unknown error occurred. Please consult the log for more details.","A system error occurred ({0})","An unknown error occurred. Please consult the log for more details.","{0} ({1} errors in total)","An unknown error occurred. Please consult the log for more details."],"vs/base/node/processes":["Can't execute a shell command on a UNC drive."]});
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/f46c4c469d6e6d8c46f268d1553c5dc4b475840f/core/vs/workbench/services/search/node/searchApp.nls.js.map
